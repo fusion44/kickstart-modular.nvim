8f9ddbfdf88411e6bacef20581b826575d34f106
fusion44 <some.fusion@gmail.com>
2023-12-30 17:58:22 +0100

    feat: remove custom gitsign signs

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -54,13 +54,6 @@ require('lazy').setup({
     'lewis6991/gitsigns.nvim',
     opts = {
       -- See `:help gitsigns.txt`
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
       on_attach = function(bufnr)
         local gs = package.loaded.gitsigns
 
