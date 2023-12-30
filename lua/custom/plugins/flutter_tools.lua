c1f4b4b884bb587e055d9156f37c656e5b133ef7
fusion44 <some.fusion@gmail.com>
2023-12-30 18:48:31 +0100

    feat: add Flutter tools plugin

---
+++ lua/custom/plugins/flutter_tools.lua
@@ -0,0 +1,27 @@
+return {
+  'akinsho/flutter-tools.nvim',
+  lazy = false,
+  opts = function()
+    return {
+      -- https://github.com/akinsho/flutter-tools.nvim/issues/178
+      settings = {
+        lineLength = (function()
+          return vim.fn.expand('%:p'):find '^/home/f44/dev/stuff/jaspr/' and 120 or 80
+        end)(),
+      },
+      debugger = {
+        enabled = true,
+        run_via_dap = true,
+        register_configurations = function(_)
+          require("dap").configurations.dart = {}
+          require("dap.ext.vscode").load_launchjs()
+        end,
+      },
+    }
+  end,
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'stevearc/dressing.nvim', -- optional for vim.ui.select
+  },
+  config = true,
+}
--- lua/treesitter-setup.lua
+++ lua/treesitter-setup.lua
@@ -4,13 +4,13 @@
 vim.defer_fn(function()
   require('nvim-treesitter.configs').setup {
     -- Add languages to be installed here that you want installed for treesitter
-    ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'bash' },
+    ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'bash', 'dart' },
 
     -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
     auto_install = false,
 
     highlight = { enable = true },
-    indent = { enable = true },
+    indent = { enable = false },
     incremental_selection = {
       enable = true,
       keymaps = {
