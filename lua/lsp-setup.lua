53167bf01ed25d1be2566f538ca6d7d919f77bb1
fusion44 <some.fusion@gmail.com>
2023-12-30 13:07:53 +0100

    feat: add lua language lsp configs

--- lua/lsp-setup.lua
+++ lua/lsp-setup.lua
@@ -68,3 +68,5 @@ require('neodev').setup()
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
+-- Require all the LSP setups
+require 'custom.lsp.lua'
