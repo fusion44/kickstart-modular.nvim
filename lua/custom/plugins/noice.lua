74493d7dd86a1b339e3925e3268c4c1659d72ef7
fusion44 <some.fusion@gmail.com>
2024-01-05 18:17:57 +0100

    feat: filter "written" messages

--- lua/custom/plugins/noice.lua
+++ lua/custom/plugins/noice.lua
@@ -30,6 +30,17 @@ return {
         inc_rename = false, -- enables an input dialog for inc-rename.nvim
         lsp_doc_border = false, -- add a border to hover docs and signature help
       },
+      routes = {
+        {
+          -- filter spammy messages on buffer write
+          filter = {
+            event = 'msg_show',
+            kind = '',
+            find = 'written',
+          },
+          opts = { skip = true },
+        },
+      },
     }
   end,
 }
